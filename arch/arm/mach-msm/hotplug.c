748153e97b810eb4748ef2385b7d3f745c1d0f92
Kostas K <mpokwsths@gmail.com>
2011-09-08 12:15:00 +0100

    ARM: SoC: convert MSM to SMP operations
    Convert MSM SMP platforms to use struct smp_operations to provide
    their SMP and CPU hotplug operations.
    
    [mpokwsths: Adapt to 8974/hammerhead]
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Acked-by: Nicolas Pitre <nico@linaro.org>
    Cc: David Brown <davidb@codeaurora.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-msm/hotplug.c
+++ arch/arm/mach-msm/hotplug.c
@@ -22,6 +22,7 @@
 
 #include "pm.h"
 #include "spm.h"
+#include "platsmp.h"
 
 extern volatile int pen_release;
 
@@ -62,14 +63,14 @@ static inline void platform_do_lowpower(unsigned int cpu, int *spurious)
 	}
 }
 
-int platform_cpu_kill(unsigned int cpu)
+int msm_cpu_kill(unsigned int cpu)
 {
-	int ret = 0;
+	int ret;
 
-	if (cpumask_test_and_clear_cpu(cpu, &cpu_dying_mask))
-		ret = msm_pm_wait_cpu_shutdown(cpu);
-
-	return ret ? 0 : 1;
+	ret = msm_pm_wait_cpu_shutdown(cpu);
+	if (ret)
+		return 0;
+	return 1;
 }
 
 /*
@@ -77,7 +78,7 @@ int platform_cpu_kill(unsigned int cpu)
  *
  * Called with IRQs disabled
  */
-void platform_cpu_die(unsigned int cpu)
+void __ref msm_cpu_die(unsigned int cpu)
 {
 	int spurious = 0;
 
@@ -97,15 +98,6 @@ void platform_cpu_die(unsigned int cpu)
 
 	if (spurious)
 		pr_warn("CPU%u: %u spurious wakeup calls\n", cpu, spurious);
-}
-
-int platform_cpu_disable(unsigned int cpu)
-{
-	/*
-	 * we don't allow CPU 0 to be shutdown (it is still too special
-	 * e.g. clock tick interrupts)
-	 */
-	return cpu == 0 ? -EPERM : 0;
 }
 
 #define CPU_SHIFT	0
