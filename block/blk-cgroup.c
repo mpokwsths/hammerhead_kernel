ed88f7160272a92914598ed5754b19248468c639
Tejun Heo <tj@kernel.org>
2012-03-19 15:10:56 -0700

    blkcg: add blkcg->id
    Add 64bit unique id to blkcg.  This will be used by policies which
    want blkcg identity test to tell whether the associated blkcg has
    changed.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -19,6 +19,7 @@
 #include <linux/slab.h>
 #include <linux/genhd.h>
 #include <linux/delay.h>
+#include <linux/atomic.h>
 #include "blk-cgroup.h"
 #include "blk.h"
 
@@ -1618,6 +1619,7 @@ static void blkiocg_destroy(struct cgroup *cgroup)
 
 static struct cgroup_subsys_state *blkiocg_create(struct cgroup *cgroup)
 {
+	static atomic64_t id_seq = ATOMIC64_INIT(0);
 	struct blkio_cgroup *blkcg;
 	struct cgroup *parent = cgroup->parent;
 
@@ -1631,6 +1633,7 @@ static struct cgroup_subsys_state *blkiocg_create(struct cgroup *cgroup)
 		return ERR_PTR(-ENOMEM);
 
 	blkcg->weight = BLKIO_WEIGHT_DEFAULT;
+	blkcg->id = atomic64_inc_return(&id_seq); /* root is 0, start from 1 */
 done:
 	spin_lock_init(&blkcg->lock);
 	INIT_HLIST_HEAD(&blkcg->blkg_list);
