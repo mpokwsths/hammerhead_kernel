d9d129bc1c3a199f0d1cbe2c091018761752cd1b
Guennadi Liakhovetski <g.liakhovetski@gmx.de>
2012-05-01 16:51:38 +0200

    mmc: core: convert slot functions to managed allocation
    This prepares for the addition of further slot functions.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    
    Conflicts:
    	drivers/mmc/core/host.c

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -11,6 +11,7 @@
 #define LINUX_MMC_HOST_H
 
 #include <linux/leds.h>
+#include <linux/mutex.h>
 #include <linux/sched.h>
 #include <linux/device.h>
 #include <linux/fault-inject.h>
@@ -194,6 +195,7 @@ struct mmc_context_info {
  * struct mmc_slot - MMC slot functions
  *
  * @cd_irq:		MMC/SD-card slot hotplug detection IRQ or -EINVAL
+ * @lock:		protect the @handler_priv pointer
  * @handler_priv:	MMC/SD-card slot context
  *
  * Some MMC/SD host controllers implement slot-functions like card and
@@ -203,6 +205,7 @@ struct mmc_context_info {
  */
 struct mmc_slot {
 	int cd_irq;
+	struct mutex lock;
 	void *handler_priv;
 };
 
