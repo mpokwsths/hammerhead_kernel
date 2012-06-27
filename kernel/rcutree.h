20f3006d691c558bdf37f96f6dc1fc58a3993ad0
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2012-06-26 17:00:35 -0700

    rcu: Prevent force_quiescent_state() memory contention
    Large systems running RCU_FAST_NO_HZ kernels see extreme memory
    contention on the rcu_state structure's ->fqslock field.  This
    can be avoided by disabling RCU_FAST_NO_HZ, either at compile time
    or at boot time (via the nohz kernel boot parameter), but large
    systems will no doubt become sensitive to energy consumption.
    This commit therefore uses a combining-tree approach to spread the
    memory contention across new cache lines in the leaf rcu_node structures.
    This can be thought of as a tournament lock that has only a try-lock
    acquisition primitive.
    
    The effect on small systems is minimal, because such systems have
    an rcu_node "tree" consisting of a single node.  In addition, this
    functionality is not used on fastpaths.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>
    
    Conflicts:
    	kernel/rcutree.h

--- kernel/rcutree.h
+++ kernel/rcutree.h
@@ -196,6 +196,8 @@ struct rcu_node {
 				/* Refused to boost: not sure why, though. */
 				/*  This can happen due to race conditions. */
 #endif /* #ifdef CONFIG_RCU_BOOST */
+				/* State of node_kthread_task for tracing. */
+	raw_spinlock_t fqslock ____cacheline_internodealigned_in_smp;
 } ____cacheline_internodealigned_in_smp;
 
 /*
