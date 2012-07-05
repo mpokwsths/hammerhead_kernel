4e27a518e7a18f250b9405a004c2f02af9cfbbbc
Daniel Drake <dsd@laptop.org>
2012-07-05 22:06:13 +0100

    mmc: sdhci: poll for card even when card is logically unremovable
    The Marvell CaFe is now marked as having bad card detection to fix
    a problem during system resume.
    
    Now on the OLPC XO-1 we are facing the issue that the card is marked
    as logically unremovable (via MMC_UNSAFE_RESUME), which means that
    mmc_card_is_removable considers the card non-removable. The existing
    code logic decides not to poll for card presence in this case, and
    card detection is also disabled because of the quirk being set.
    This means that no SD cards are detected when inserted after boot.
    
    Refine the logic to enable card presence polling in the case when
    a card is logically unremovable, only avoiding the poll in the case
    when the card is physically non-removable (denoted with
    MMC_CAP_NONREMOVABLE).
    
    Signed-off-by: Daniel Drake <dsd@laptop.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -3284,7 +3284,7 @@ int sdhci_add_host(struct sdhci_host *host)
 		mmc->caps |= MMC_CAP_SD_HIGHSPEED | MMC_CAP_MMC_HIGHSPEED;
 
 	if ((host->quirks & SDHCI_QUIRK_BROKEN_CARD_DETECTION) &&
-	    mmc_card_is_removable(mmc))
+	    !(host->mmc->caps & MMC_CAP_NONREMOVABLE))
 		mmc->caps |= MMC_CAP_NEEDS_POLL;
 
 	if (host->quirks2 & SDHCI_QUIRK2_NO_1_8_V)
