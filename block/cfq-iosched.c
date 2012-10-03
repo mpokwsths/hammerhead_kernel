2da57bbc5ae5049e306428b56f2dd302a530c0b3
Vivek Goyal <vgoyal@redhat.com>
2012-10-03 16:56:59 -0400

    cfq-iosched: Rename few functions related to selecting workload
    choose_service_tree() selects/sets both wl_class and wl_type.  Rename it to
    choose_wl_class_and_type() to make it very clear.
    
    cfq_choose_wl() only selects and sets wl_type. It is easy to confuse
    it with choose_st(). So rename it to cfq_choose_wl_type() to make
    it clear what does it do.
    
    Just renaming. No functionality change.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Acked-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -2501,7 +2501,7 @@ static void cfq_setup_merge(struct cfq_queue *cfqq, struct cfq_queue *new_cfqq)
 	}
 }
 
-static enum wl_type_t cfq_choose_wl(struct cfq_data *cfqd,
+static enum wl_type_t cfq_choose_wl_type(struct cfq_data *cfqd,
 			struct cfq_group *cfqg, enum wl_class_t wl_class)
 {
 	struct cfq_queue *queue;
@@ -2524,7 +2524,8 @@ static enum wl_type_t cfq_choose_wl(struct cfq_data *cfqd,
 	return cur_best;
 }
 
-static void choose_service_tree(struct cfq_data *cfqd, struct cfq_group *cfqg)
+static void
+choose_wl_class_and_type(struct cfq_data *cfqd, struct cfq_group *cfqg)
 {
 	unsigned slice;
 	unsigned count;
@@ -2562,7 +2563,7 @@ static void choose_service_tree(struct cfq_data *cfqd, struct cfq_group *cfqg)
 
 new_workload:
 	/* otherwise select new workload type */
-	cfqd->serving_wl_type = cfq_choose_wl(cfqd, cfqg,
+	cfqd->serving_wl_type = cfq_choose_wl_type(cfqd, cfqg,
 					cfqd->serving_wl_class);
 	st = st_for(cfqg, cfqd->serving_wl_class, cfqd->serving_wl_type);
 	count = st->count;
@@ -2635,7 +2636,7 @@ static void cfq_choose_cfqg(struct cfq_data *cfqd)
 	} else
 		cfqd->workload_expires = jiffies - 1;
 
-	choose_service_tree(cfqd, cfqg);
+	choose_wl_class_and_type(cfqd, cfqg);
 }
 
 /*
