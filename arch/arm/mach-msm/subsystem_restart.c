f54db976e525cf2899a77126612842b912682c89
Deepak Katragadda <dkatraga@codeaurora.org>
2012-10-04 14:21:07 -0700

    msm: SSR: Merge start/stop ops with powerup/shutdown ops
    The start/stop ops were introduced to provide a clear migration
    path for merging SSR drivers and PIL drivers together. Now that
    the merge is over we can drop the start/stop ops and use the
    powerup/shutdown ops in their place. Mostly this means we disable
    the irq during probe so that powerup() can enable it and then
    drop the start/stop callbacks.
    
    Change-Id: I787b60a93a31a4085c4118292ea8a163df175765
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: Deepak Katragadda <dkatraga@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/pil-pronto.c
    	arch/arm/mach-msm/pil-q6v5-lpass.c
    	arch/arm/mach-msm/pil-q6v5-mss.c
    	arch/arm/mach-msm/pil-riva.c
    	arch/arm/mach-msm/subsystem_restart.c
    	sound/soc/codecs/msm8x10-wcd.c

--- arch/arm/mach-msm/subsystem_restart.c
+++ arch/arm/mach-msm/subsystem_restart.c
@@ -449,7 +449,7 @@ static void subsystem_shutdown(struct subsys_device *dev, void *data)
 	const char *name = dev->desc->name;
 
 	pr_info("[%p]: Shutting down %s\n", current, name);
-	if (dev->desc->shutdown(dev->desc) < 0) {
+	if (dev->desc->shutdown(dev->desc, true) < 0) {
 #ifdef CONFIG_LGE_HANDLE_PANIC
 		lge_set_magic_subsystem(name, LGE_ERR_SUB_SD);
 #endif
@@ -517,8 +517,11 @@ static int subsys_start(struct subsys_device *subsys)
 {
 	int ret;
 
+	notify_each_subsys_device(&subsys, 1, SUBSYS_BEFORE_POWERUP,
+								NULL);
+
 	init_completion(&subsys->err_ready);
-	ret = subsys->desc->start(subsys->desc);
+	ret = subsys->desc->powerup(subsys->desc);
 	if (ret)
 		return ret;
 
@@ -528,21 +531,27 @@ static int subsys_start(struct subsys_device *subsys)
 	}
 
 	ret = wait_for_err_ready(subsys);
-	if (ret)
+	if (ret) {
 		/* pil-boot succeeded but we need to shutdown
 		 * the device because error ready timed out.
 		 */
-		subsys->desc->stop(subsys->desc);
-	else
+		subsys->desc->shutdown(subsys->desc, false);
+		return ret;
+	} else {
 		subsys_set_state(subsys, SUBSYS_ONLINE);
+	}
 
+	notify_each_subsys_device(&subsys, 1, SUBSYS_AFTER_POWERUP,
+								NULL);
 	return ret;
 }
 
 static void subsys_stop(struct subsys_device *subsys)
 {
-	subsys->desc->stop(subsys->desc);
+	notify_each_subsys_device(&subsys, 1, SUBSYS_BEFORE_SHUTDOWN, NULL);
+	subsys->desc->shutdown(subsys->desc, false);
 	subsys_set_state(subsys, SUBSYS_OFFLINE);
+	notify_each_subsys_device(&subsys, 1, SUBSYS_AFTER_SHUTDOWN, NULL);
 }
 
 static struct subsys_tracking *subsys_get_track(struct subsys_device *subsys)
@@ -1139,6 +1148,7 @@ static int subsys_setup_irqs(struct subsys_device *subsys)
 				desc->name, ret);
 			return ret;
 		}
+		disable_irq(desc->wdog_bite_irq);
 	}
 
 	if (desc->err_ready_irq) {
