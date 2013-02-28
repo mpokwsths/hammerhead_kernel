cb601f164d3358c41c06dfb667852ed43c704de2
Will Deacon <will.deacon@arm.com>
2013-02-28 17:48:40 +0100

    ARM: 7661/1: mm: perform explicit branch predictor maintenance when required
    The ARM ARM requires branch predictor maintenance if, for a given ASID,
    the instructions at a specific virtual address appear to change.
    
    From the kernel's point of view, that means:
    
    	- Changing the kernel's view of memory (e.g. switching to the
    	  identity map)
    	- ASID rollover (since ASIDs will be re-allocated to new tasks)
    
    This patch adds explicit branch predictor maintenance when either of the
    two conditions above are met.
    
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    Conflicts:
    	arch/arm/kernel/smp.c

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -343,6 +343,7 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 	current->active_mm = mm;
 	cpumask_set_cpu(cpu, mm_cpumask(mm));
 	cpu_switch_mm(mm->pgd, mm);
+	local_flush_bp_all();
 	enter_lazy_tlb(mm, current);
 	local_flush_tlb_all();
 
