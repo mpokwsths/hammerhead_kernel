884b1c92c9f4841f37f4e19c5295e7099f37f60d
Deepak Katragadda <dkatraga@codeaurora.org>
2013-08-29 18:21:53 -0700

    msm: pil: Replace disable_irq_nosync with disable_irq wherever required
    Limit usage of disable_irq_nosync by using it only in interrupt
    handler functions. Otherwise, use disable_irq. This could help
    remove any race conditions which might occur due to _nosync.
    
    Change-Id: Ie783830963fcb800bc50f74b0c29314fd9fa526a
    Signed-off-by: Deepak Katragadda <dkatraga@codeaurora.org>

--- arch/arm/mach-msm/pil-pronto.c
+++ arch/arm/mach-msm/pil-pronto.c
@@ -340,7 +340,7 @@ static int wcnss_shutdown(const struct subsys_desc *subsys, bool force_stop)
 {
 	struct pronto_data *drv = subsys_to_drv(subsys);
 
-	disable_irq_nosync(drv->subsys_desc.wdog_bite_irq);
+	disable_irq(drv->subsys_desc.wdog_bite_irq);
 	pil_shutdown(&drv->desc);
 	flush_delayed_work(&drv->cancel_vote_work);
 	wcnss_flush_delayed_boot_votes();
