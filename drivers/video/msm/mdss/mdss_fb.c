4dfb6e92c9cd73320767187a94d6e685729a83da
Terence Hampson <thampson@codeaurora.org>
2013-09-27 16:31:12 -0400

    mdss: fb: Unblock blit ioctl calls when commit is occuring
    Since BLIT calls have no effect on the frame being commited it
    does not need to block in pan_idle.
    
    Change-Id: Ic0cfa7c0c275ddc051b60ff8823fcd7299cb8b57
    Signed-off-by: Terence Hampson <thampson@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_fb.c

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -2082,7 +2082,8 @@ static int mdss_fb_ioctl(struct fb_info *info, unsigned int cmd,
 	mfd = (struct msm_fb_data_type *)info->par;
 	mdss_fb_power_setting_idle(mfd);
 
-	if (cmd != MSMFB_VSYNC_CTRL && cmd != MSMFB_OVERLAY_VSYNC_CTRL) {
+	if ((cmd != MSMFB_VSYNC_CTRL) && (cmd != MSMFB_OVERLAY_VSYNC_CTRL) &&
+			(cmd != MSMFB_ASYNC_BLIT) && (cmd != MSMFB_BLIT)) {
 		ret = mdss_fb_pan_idle(mfd);
 		if (ret) {
 			pr_debug("Shutdown pending. Aborting operation %x\n",
