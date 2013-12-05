b6fadc9b43f518b8b46ccd2f0a19df7ccb007871
Kumar Gala <galak@codeaurora.org>
2013-12-05 14:29:20 -0600

    msm: remove setup and last remnants of MSM_IMEM_BASE
    Now that there are no users of MSM_IMEM_BASE we can remove the setup
    code associated and any remaining references to it.
    
    Change-Id: I38d099e448f87af1fd2f140505dde7712f9d28b8
    Signed-off-by: Kumar Gala <galak@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/board-dt.c

--- arch/arm/mach-msm/io.c
+++ arch/arm/mach-msm/io.c
@@ -54,7 +54,6 @@ static struct map_desc msm_8974_io_desc[] __initdata = {
 void __init msm_map_8974_io(void)
 {
 	iotable_init(msm_8974_io_desc, ARRAY_SIZE(msm_8974_io_desc));
-	of_scan_flat_dt(msm_scan_dt_map_imem, NULL);
 }
 #endif /* CONFIG_ARCH_MSM8974 */
 
@@ -69,7 +68,6 @@ static struct map_desc msm_8084_io_desc[] __initdata = {
 void __init msm_map_8084_io(void)
 {
 	iotable_init(msm_8084_io_desc, ARRAY_SIZE(msm_8084_io_desc));
-	of_scan_flat_dt(msm_scan_dt_map_imem, NULL);
 }
 #endif /* CONFIG_ARCH_APQ8084 */
 
@@ -84,7 +82,6 @@ static struct map_desc fsm9900_io_desc[] __initdata = {
 void __init msm_map_fsm9900_io(void)
 {
 	iotable_init(fsm9900_io_desc, ARRAY_SIZE(fsm9900_io_desc));
-	of_scan_flat_dt(msm_scan_dt_map_imem, NULL);
 }
 #endif /* CONFIG_ARCH_FSM9900 */
 
@@ -97,7 +94,6 @@ static struct map_desc msmkrypton_io_desc[] __initdata = {
 void __init msm_map_msmkrypton_io(void)
 {
 	iotable_init(msmkrypton_io_desc, ARRAY_SIZE(msmkrypton_io_desc));
-	of_scan_flat_dt(msm_scan_dt_map_imem, NULL);
 }
 #endif /* CONFIG_ARCH_MSMKRYPTON */
 
@@ -112,7 +108,6 @@ static struct map_desc mpq8092_io_desc[] __initdata = {
 void __init msm_map_mpq8092_io(void)
 {
 	iotable_init(mpq8092_io_desc, ARRAY_SIZE(mpq8092_io_desc));
-	of_scan_flat_dt(msm_scan_dt_map_imem, NULL);
 }
 #endif /* CONFIG_ARCH_MPQ8092 */
 
@@ -130,7 +125,6 @@ static struct map_desc msm_8226_io_desc[] __initdata = {
 void __init msm_map_msm8226_io(void)
 {
 	iotable_init(msm_8226_io_desc, ARRAY_SIZE(msm_8226_io_desc));
-	of_scan_flat_dt(msm_scan_dt_map_imem, NULL);
 }
 #endif /* CONFIG_ARCH_MSM8226 */
 
@@ -144,7 +138,6 @@ static struct map_desc msm8610_io_desc[] __initdata = {
 void __init msm_map_msm8610_io(void)
 {
 	iotable_init(msm8610_io_desc, ARRAY_SIZE(msm8610_io_desc));
-	of_scan_flat_dt(msm_scan_dt_map_imem, NULL);
 }
 #endif /* CONFIG_ARCH_MSM8610 */
 
@@ -160,6 +153,5 @@ static struct map_desc msmsamarium_io_desc[] __initdata = {
 void __init msm_map_msmsamarium_io(void)
 {
 	iotable_init(msmsamarium_io_desc, ARRAY_SIZE(msmsamarium_io_desc));
-	of_scan_flat_dt(msm_scan_dt_map_imem, NULL);
 }
 #endif /* CONFIG_ARCH_MSMSAMARIUM */
