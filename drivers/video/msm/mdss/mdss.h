9eeb7d2f100a5be5984c8448f5a0fb749c0acd26
Huaibin Yang <huaibiny@codeaurora.org>
2013-12-09 16:48:24 -0800

    msm: mdss: support mdp prefill bandwith for multiple platforms
    To calculate mdp prefill bandwidth, mdp driver needs to get h/w
    dependent buffer constants from device tree. Also add h/w feature
    dependent logic.
    
    Change-Id: I8d56ec502a6fd56eaae5b65a326ef966be46f14b
    Signed-off-by: Huaibin Yang <huaibiny@codeaurora.org>
    
    Conflicts:
    	arch/arm/boot/dts/qcom/apq8084-mdss.dtsi
    	arch/arm/boot/dts/qcom/msm8226-mdss.dtsi
    	arch/arm/boot/dts/qcom/msm8974-mdss.dtsi
    	drivers/video/msm/mdss/mdss.h

--- drivers/video/msm/mdss/mdss.h
+++ drivers/video/msm/mdss/mdss.h
@@ -63,6 +63,16 @@ struct mdss_fudge_factor {
 	u32 denom;
 };
 
+struct mdss_prefill_data {
+	u32 ot_bytes;
+	u32 y_buf_bytes;
+	u32 y_scaler_lines_bilinear;
+	u32 y_scaler_lines_caf;
+	u32 post_scaler_pixels;
+	u32 pp_pixels;
+	u32 fbc_lines;
+};
+
 struct mdss_data_type {
 	u32 mdp_rev;
 	struct clk *mdp_clk[MDSS_MAX_CLK];
@@ -140,6 +150,7 @@ struct mdss_data_type {
 	struct early_suspend early_suspend;
 	void *debug_data;
 	bool mixer_switched;
+	struct mdss_prefill_data prefill_data;
 
 	int handoff_pending;
 };
