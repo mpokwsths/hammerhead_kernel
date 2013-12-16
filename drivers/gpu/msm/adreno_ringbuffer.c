7e7a57cf973bd3fc397ca8fef10a18a2b597c882
Jordan Crouse <jcrouse@codeaurora.org>
2013-12-16 14:16:06 -0700

    msm: kgsl: Use the correct method for exporting trace event functions
    Use EXPORT_TRACEPOINT_SYMBOL instead of my lazy "trace" wrappers for
    trace_kgsl_regwrite and trace_kgsl_issueibcmds thus saving lots of
    code and a shred or two of dignity along the way.
    
    Change-Id: Ic0dedbad4f54a7522edd2f9c3d017ba1e9dba505
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -1316,7 +1316,7 @@ int adreno_ringbuffer_submitcmd(struct adreno_device *adreno_dev,
 
 done:
 	device->pwrctrl.irq_last = 0;
-	kgsl_trace_issueibcmds(device, context->id, cmdbatch,
+	trace_kgsl_issueibcmds(device, context->id, cmdbatch,
 		cmdbatch->timestamp, cmdbatch->flags, ret,
 		drawctxt->type);
 
