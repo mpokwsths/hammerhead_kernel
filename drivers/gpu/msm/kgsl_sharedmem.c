b61582905d9c28f6d7a520f7ae4f9971e700fbf1
Shrenuj Bansal <shrenujb@codeaurora.org>
2014-01-16 18:34:46 -0800

    msm: kgsl: Add struct kgsl_device to kgsl_allocate_contiguous API
    We need to pass a non NULL struct device * for the call to
    dma_alloc_coherent(). Change certain APIs to allow us to pass
    down struct kgsl_device ptr so we can pass in a valid struct
    device * to dma_alloc_coherent()
    
    Change-Id: I24eaa2b7834b573b0520f6aa1f00447eb1f35dc6
    Signed-off-by: Shrenuj Bansal <shrenujb@codeaurora.org>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -482,7 +482,7 @@ static int kgsl_contiguous_vmfault(struct kgsl_memdesc *memdesc,
 static void kgsl_coherent_free(struct kgsl_memdesc *memdesc)
 {
 	kgsl_driver.stats.coherent -= memdesc->size;
-	dma_free_coherent(NULL, memdesc->size,
+	dma_free_coherent(memdesc->dev, memdesc->size,
 			  memdesc->hostptr, memdesc->physaddr);
 }
 
@@ -748,7 +748,8 @@ kgsl_sharedmem_page_alloc_user(struct kgsl_memdesc *memdesc,
 EXPORT_SYMBOL(kgsl_sharedmem_page_alloc_user);
 
 int
-kgsl_sharedmem_alloc_coherent(struct kgsl_memdesc *memdesc, size_t size)
+kgsl_sharedmem_alloc_coherent(struct kgsl_device *device,
+			struct kgsl_memdesc *memdesc, size_t size)
 {
 	int result = 0;
 
@@ -758,9 +759,10 @@ kgsl_sharedmem_alloc_coherent(struct kgsl_memdesc *memdesc, size_t size)
 
 	memdesc->size = size;
 	memdesc->ops = &kgsl_coherent_ops;
+	memdesc->dev = device->parentdev;
 
-	memdesc->hostptr = dma_alloc_coherent(NULL, size, &memdesc->physaddr,
-					      GFP_KERNEL);
+	memdesc->hostptr = dma_alloc_coherent(memdesc->dev, size,
+					&memdesc->physaddr, GFP_KERNEL);
 	if (memdesc->hostptr == NULL) {
 		result = -ENOMEM;
 		goto err;
