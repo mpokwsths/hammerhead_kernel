842b8d32a2c9d989e594e4cea042e5e16c2c12bb
Laura Abbott <lauraa@codeaurora.org>
2014-02-04 13:59:50 -0800

    msm: Remove cache wrappers
    Having cache wrappers is no longer needed. Clients can
    just call the caching APIs directly. Remove the wrappers.
    
    Change-Id: I50faaca0d913f14218320d026b4bf1ecf2c30672
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/memory.c
    
    Conflicts:
    	arch/arm/mach-msm/include/mach/memory.h
    	arch/arm/mach-msm/memory.c

--- arch/arm/mach-msm/memory.c
+++ arch/arm/mach-msm/memory.c
@@ -40,10 +40,6 @@
 #include <linux/sched.h>
 #include <linux/of_fdt.h>
 
-/* fixme */
-#include <asm/tlbflush.h>
-#include <../../mm/mm.h>
-
 #if defined(CONFIG_ARCH_MSM7X27)
 static void *strongly_ordered_page;
 static char strongly_ordered_mem[PAGE_SIZE*2-4];
@@ -80,31 +76,6 @@ void write_to_strongly_ordered_memory(void)
 void write_to_strongly_ordered_memory(void) { }
 #endif
 EXPORT_SYMBOL(write_to_strongly_ordered_memory);
-
-/* These cache related routines make the assumption (if outer cache is
- * available) that the associated physical memory is contiguous.
- * They will operate on all (L1 and L2 if present) caches.
- */
-void clean_and_invalidate_caches(unsigned long vstart,
-	unsigned long length, unsigned long pstart)
-{
-	dmac_flush_range((void *)vstart, (void *) (vstart + length));
-	outer_flush_range(pstart, pstart + length);
-}
-
-void clean_caches(unsigned long vstart,
-	unsigned long length, unsigned long pstart)
-{
-	dmac_clean_range((void *)vstart, (void *) (vstart + length));
-	outer_clean_range(pstart, pstart + length);
-}
-
-void invalidate_caches(unsigned long vstart,
-	unsigned long length, unsigned long pstart)
-{
-	dmac_inv_range((void *)vstart, (void *) (vstart + length));
-	outer_inv_range(pstart, pstart + length);
-}
 
 char *memtype_name[] = {
 	"SMI_KERNEL",
