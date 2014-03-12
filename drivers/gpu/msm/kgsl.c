f6413e47d484a477b2bea7d52cc6dd06710c5ce5
Jordan Crouse <jcrouse@codeaurora.org>
2014-03-11 19:44:24 -0600

    msm: kgsl: Remove some spammy log messages
    We should not be printing a log message every time the KGSL device
    is opened, nor should we print a log message for properties that
    are intentionally not available on some targets.
    
    Change-Id: Ic0dedbadb75338e8b5fa7a414733160de5049fa9
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -1145,8 +1145,6 @@ static int kgsl_open(struct inode *inodep, struct file *filep)
 		goto err_stop;
 	}
 
-	dev_info(device->dev, "Initialized %s: mmu=%s\n", device->name,
-		kgsl_mmu_enabled() ? "on" : "off");
 
 	return result;
 
@@ -4258,6 +4256,9 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 
 	/* Initialize common sysfs entries */
 	kgsl_pwrctrl_init_sysfs(device);
+
+	dev_info(device->dev, "Initialized %s: mmu=%s\n", device->name,
+		kgsl_mmu_enabled() ? "on" : "off");
 
 	return 0;
 
