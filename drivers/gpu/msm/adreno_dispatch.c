9baa910797cea9e9f0e5a51b3d4f6d7ce549fdd2
Carter Cooper <ccooper@codeaurora.org>
2014-03-19 09:22:23 -0600

    msm: kgsl: Add IFH feature by removing IB commands
    Removing the IBs from being sent to the ringbuffer limits the
    actual amount of work that the GPU executes. This allows for
    a more realistic way to show where the CPU bottlenecks are. By
    removing just the IBs and not modifiying anything else, the GPU
    still executes some basic commands.  More importantly, it will
    still execute the interrupt path which will give more realistic
    representations of where the CPU actually spends time.
    
    CRs-fixed: 628252
    Change-Id: Ica195e2c9dc09e2fde730fe15e8bbbdf3f71672f
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>
    
    Conflicts:
    	drivers/gpu/msm/adreno_dispatch.c
    	include/uapi/linux/msm_kgsl.h

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -654,6 +654,11 @@ int adreno_dispatcher_queue_cmd(struct adreno_device *adreno_dev,
 	if (test_and_clear_bit(ADRENO_CONTEXT_FORCE_PREAMBLE, &drawctxt->priv))
 		set_bit(CMDBATCH_FLAG_FORCE_PREAMBLE, &cmdbatch->priv);
 
+
+	/* Skip this cmdbatch commands if IFH_NOP is enabled */
+	if (drawctxt->base.flags & KGSL_CONTEXT_IFH_NOP)
+		set_bit(CMDBATCH_FLAG_SKIP, &cmdbatch->priv);
+
 	/*
 	 * If we are waiting for the end of frame and it hasn't appeared yet,
 	 * then mark the command batch as skipped.  It will still progress
