f645b249dc80207eec577487765fab7068339b48
Tarun Karra <tkarra@codeaurora.org>
2014-03-21 12:58:22 -0700

    msm: kgsl: Merge some msm-3.10 changes
    Change-Id: I82e766f4869c737c1f5b73934eacaada40d46e10
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>
    
    Conflicts:
    	drivers/gpu/msm/kgsl_iommu.c

--- drivers/gpu/msm/kgsl_iommu.c
+++ drivers/gpu/msm/kgsl_iommu.c
@@ -1693,6 +1693,13 @@ done:
 	return status;
 }
 
+/*
+ * kgsl_iommu_flush_tlb_pt_current - Flush IOMMU TLB if pagetable is
+ * currently used by GPU.
+ * @pt - Pointer to kgsl pagetable structure
+ *
+ * Return - void
+ */
 static void kgsl_iommu_flush_tlb_pt_current(struct kgsl_pagetable *pt)
 {
 	int lock_taken = 0;
@@ -1768,6 +1775,8 @@ kgsl_iommu_map(struct kgsl_pagetable *pt,
 	unsigned int iommu_virt_addr;
 	struct kgsl_iommu_pt *iommu_pt = pt->priv;
 	size_t size = memdesc->size;
+	struct kgsl_device *device = pt->mmu->device;
+	struct adreno_device *adreno_dev = ADRENO_DEVICE(device);
 
 	BUG_ON(NULL == iommu_pt);
 
@@ -1809,7 +1818,7 @@ kgsl_iommu_map(struct kgsl_pagetable *pt,
 	 *  being read) -> causing TLB sync stuck issues. As a result SW must
 	 *  implement the invalidate+map.
 	 */
-	if (!msm_soc_version_supports_iommu_v0())
+	if (adreno_dev->features & IOMMU_FLUSH_TLB_ON_MAP)
 		kgsl_iommu_flush_tlb_pt_current(pt);
 
 	return ret;
