d5ffbc009eaad0641e68cfe796ae75863f6021d7
Gioh Kim <gioh.kim@lge.com>
2014-04-25 08:24:24 +0900

    Staging: android: ion: duplicated clearing of sg_table
    Because sg_table is cleared in sg_alloc_table via memset we don't need to use
    kzalloc to allocate sg_table.
    
    Signed-off-by: Gioh Kim <gioh.kim@lge.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	drivers/staging/android/ion/ion_carveout_heap.c
    	drivers/staging/android/ion/ion_chunk_heap.c
    	drivers/staging/android/ion/ion_system_heap.c

--- drivers/gpu/ion/ion_system_heap.c
+++ drivers/gpu/ion/ion_system_heap.c
@@ -186,7 +186,7 @@ static int ion_system_heap_allocate(struct ion_heap *heap,
 		i++;
 	}
 
-	table = kzalloc(sizeof(struct sg_table), GFP_KERNEL);
+	table = kmalloc(sizeof(struct sg_table), GFP_KERNEL);
 	if (!table)
 		goto err;
 
@@ -377,7 +377,7 @@ struct sg_table *ion_system_contig_heap_map_dma(struct ion_heap *heap,
 	struct sg_table *table;
 	int ret;
 
-	table = kzalloc(sizeof(struct sg_table), GFP_KERNEL);
+	table = kmalloc(sizeof(struct sg_table), GFP_KERNEL);
 	if (!table)
 		return ERR_PTR(-ENOMEM);
 	ret = sg_alloc_table(table, 1, GFP_KERNEL);
