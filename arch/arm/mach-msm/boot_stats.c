CAF-staging/msm8916_kernel

msm: boot_stats: remove use of MSM_IMEM_BASE
Move to getting address for boot_stats region of IMEM from the device tree
and mapping via ioremap.  This removes a usage of MSM_IMEM_BASE which will
allow removal of the static IO mappings.

Additionally move to using io accessors since we are accessing IO memory
instead of direct pointer manipulation.

Change-Id: Ib3d3494612b37e7ee9903d270c80ea929d6a7435
Signed-off-by: Kumar Gala <galak@codeaurora.org>

arch/arm/mach-msm/boot_stats.c
@@ -25,26 +25,34 @@
 #include <linux/of_address.h>
 #include <mach/msm_iomap.h>
 
-#include "boot_stats.h"
-
-#define MSM_BOOT_STATS_IMEM_START	(MSM_IMEM_BASE+0x6b0)
+struct boot_stats {
+	uint32_t bootloader_start;
+	uint32_t bootloader_end;
+	uint32_t bootloader_display;
+	uint32_t bootloader_load_kernel;
+};
 
 static void __iomem *mpm_counter_base;
 static uint32_t mpm_counter_freq;
-static struct boot_stats *boot_stats =
-				(void __iomem *)(MSM_BOOT_STATS_IMEM_START);
-
-static const struct of_device_id mpm_counter_of_match[]	= {
-	{ .compatible	= "qti,mpm2-sleep-counter",	},
-	{},
-};
+static struct boot_stats __iomem *boot_stats;
 
 static int mpm_parse_dt(void)
 {
 	struct device_node *np;
 	u32 freq;
 
-	np = of_find_matching_node(NULL, mpm_counter_of_match);
+	np = of_find_compatible_node(NULL, NULL, "qti,msm-imem-boot_stats");
+	if (!np) {
+		pr_err("can't find qti,msm-imem node\n");
+		return -ENODEV;
+	}
+	boot_stats = of_iomap(np, 0);
+	if (!boot_stats) {
+		pr_err("boot_stats: Can't map imem\n");
+		return -ENODEV;
+	}
+
+	np = of_find_compatible_node(NULL, NULL, "qti,mpm2-sleep-counter");
 	if (!np) {
 		pr_err("mpm_counter: can't find DT node\n");
 		return -ENODEV;
@@ -69,31 +77,31 @@ static int mpm_parse_dt(void)
 static void print_boot_stats(void)
 {
 	pr_info("KPI: Bootloader start count = %u\n",
-			boot_stats->bootloader_start);
+		readl_relaxed(&boot_stats->bootloader_start));
 	pr_info("KPI: Bootloader end count = %u\n",
-			boot_stats->bootloader_end);
+		readl_relaxed(&boot_stats->bootloader_end));
 	pr_info("KPI: Bootloader display count = %u\n",
-			boot_stats->bootloader_display);
+		readl_relaxed(&boot_stats->bootloader_display));
 	pr_info("KPI: Bootloader load kernel count = %u\n",
-			boot_stats->bootloader_load_kernel);
+		readl_relaxed(&boot_stats->bootloader_load_kernel));
 	pr_info("KPI: Kernel MPM timestamp = %u\n",
-			__raw_readl(mpm_counter_base));
+		readl_relaxed(mpm_counter_base));
 	pr_info("KPI: Kernel MPM Clock frequency = %u\n",
-			mpm_counter_freq);
+		mpm_counter_freq);
 }
 
 int boot_stats_init(void)
 {
 	int ret;
 
-	if (!boot_stats)
-		return -ENODEV;
-
 	ret = mpm_parse_dt();
 	if (ret < 0)
 		return -ENODEV;
 
 	print_boot_stats();
+
+	iounmap(boot_stats);
+	iounmap(mpm_counter_base);
 
 	return 0;
 }

mpokwsths/hammerhead_kernel

Conflicts:
	arch/arm/boot/dts/qti/apq8084.dtsi
	arch/arm/boot/dts/qti/fsm9900.dtsi
	arch/arm/boot/dts/qti/mpq8092.dtsi
	arch/arm/boot/dts/qti/msm8226.dtsi
	arch/arm/boot/dts/qti/msm8610.dtsi
	arch/arm/boot/dts/qti/msm8974-v1.dtsi
	arch/arm/boot/dts/qti/msm8974-v2.dtsi
	arch/arm/boot/dts/qti/msm8974pro.dtsi
	arch/arm/boot/dts/qti/msmkrypton.dtsi
	arch/arm/boot/dts/qti/msmsamarium.dtsi
	arch/arm/mach-msm/boot_stats.c

arch/arm/mach-msm/boot_stats.c
@@ -25,26 +25,34 @@
 #include <linux/of_address.h>
 #include <mach/msm_iomap.h>
 
-#include "boot_stats.h"
-
-#define MSM_BOOT_STATS_IMEM_START	(MSM_IMEM_BASE+0x6b0)
+struct boot_stats {
+	uint32_t bootloader_start;
+	uint32_t bootloader_end;
+	uint32_t bootloader_display;
+	uint32_t bootloader_load_kernel;
+};
 
 static void __iomem *mpm_counter_base;
 static uint32_t mpm_counter_freq;
-static struct boot_stats *boot_stats =
-				(void __iomem *)(MSM_BOOT_STATS_IMEM_START);
-
-static const struct of_device_id mpm_counter_of_match[]	= {
-	{ .compatible	= "qcom,mpm2-sleep-counter",	},
-	{},
-};
+static struct boot_stats __iomem *boot_stats;
 
 static int mpm_parse_dt(void)
 {
 	struct device_node *np;
 	u32 freq;
 
-	np = of_find_matching_node(NULL, mpm_counter_of_match);
+	np = of_find_compatible_node(NULL, NULL, "qcom,msm-imem-boot_stats");
+	if (!np) {
+		pr_err("can't find qcom,msm-imem node\n");
+		return -ENODEV;
+	}
+	boot_stats = of_iomap(np, 0);
+	if (!boot_stats) {
+		pr_err("boot_stats: Can't map imem\n");
+		return -ENODEV;
+	}
+
+	np = of_find_compatible_node(NULL, NULL, "qcom,mpm2-sleep-counter");
 	if (!np) {
 		pr_err("mpm_counter: can't find DT node\n");
 		return -ENODEV;
@@ -69,31 +77,31 @@ static int mpm_parse_dt(void)
 static void print_boot_stats(void)
 {
 	pr_info("KPI: Bootloader start count = %u\n",
-			boot_stats->bootloader_start);
+		readl_relaxed(&boot_stats->bootloader_start));
 	pr_info("KPI: Bootloader end count = %u\n",
-			boot_stats->bootloader_end);
+		readl_relaxed(&boot_stats->bootloader_end));
 	pr_info("KPI: Bootloader display count = %u\n",
-			boot_stats->bootloader_display);
+		readl_relaxed(&boot_stats->bootloader_display));
 	pr_info("KPI: Bootloader load kernel count = %u\n",
-			boot_stats->bootloader_load_kernel);
+		readl_relaxed(&boot_stats->bootloader_load_kernel));
 	pr_info("KPI: Kernel MPM timestamp = %u\n",
-			__raw_readl(mpm_counter_base));
+		readl_relaxed(mpm_counter_base));
 	pr_info("KPI: Kernel MPM Clock frequency = %u\n",
-			mpm_counter_freq);
+		mpm_counter_freq);
 }
 
 int boot_stats_init(void)
 {
 	int ret;
 
-	if (!boot_stats)
-		return -ENODEV;
-
 	ret = mpm_parse_dt();
 	if (ret < 0)
 		return -ENODEV;
 
 	print_boot_stats();
+
+	iounmap(boot_stats);
+	iounmap(mpm_counter_base);
 
 	return 0;
 }