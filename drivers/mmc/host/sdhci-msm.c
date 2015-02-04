a12f265e79c53a07b56ccf41367b370ba2f35406
Dov Levenglick <dovl@codeaurora.org>
2015-02-04 17:07:00 +0200

    mmc: add support for scheduling mmcqd on idle CPU
    In order to boost mmc performance on various platforms,
    add support for configuring whether set_wake_up_idle()
    should be called on the mmc queue thread (mmcqd).
    The decision will be set in each individual platform's
    dts file.
    
    CRs-Fixed: 787554
    Change-Id: I3989d3f5b8228785e6d3bc49c7eb01ebf2fa2f38
    Signed-off-by: Dov Levenglick <dovl@codeaurora.org>
    
    Conflicts:
    	include/linux/mmc/host.h

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -1194,7 +1194,9 @@ static void sdhci_msm_populate_affinity(struct sdhci_msm_pltfm_data *pdata,
 #endif
 
 /* Parse platform data */
-static struct sdhci_msm_pltfm_data *sdhci_msm_populate_pdata(struct device *dev)
+static
+struct sdhci_msm_pltfm_data *sdhci_msm_populate_pdata(struct device *dev,
+						struct sdhci_msm_host *msm_host)
 {
 	struct sdhci_msm_pltfm_data *pdata = NULL;
 	struct device_node *np = dev->of_node;
@@ -1341,6 +1343,9 @@ static struct sdhci_msm_pltfm_data *sdhci_msm_populate_pdata(struct device *dev)
 		pdata->mpm_sdiowakeup_int = -1;
 
 	sdhci_msm_populate_affinity(pdata, np);
+
+	if (of_property_read_bool(np, "qcom,wakeup-on-idle"))
+		msm_host->mmc->wakeup_on_idle = true;
 
 	return pdata;
 out:
@@ -2556,7 +2561,8 @@ static int __devinit sdhci_msm_probe(struct platform_device *pdev)
 			goto pltfm_free;
 		}
 
-		msm_host->pdata = sdhci_msm_populate_pdata(&pdev->dev);
+		msm_host->pdata = sdhci_msm_populate_pdata(&pdev->dev,
+							   msm_host);
 		if (!msm_host->pdata) {
 			dev_err(&pdev->dev, "DT parsing error\n");
 			goto pltfm_free;
