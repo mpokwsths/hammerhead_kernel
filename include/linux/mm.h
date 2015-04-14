b8ba52af0917f8cd54ab007c2b0662f5f6807cc8
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2015-04-14 15:45:30 -0700

    mm: hide per-cpu lists in output of show_mem()
    This makes show_mem() much less verbose on huge machines.  Instead of huge
    and almost useless dump of counters for each per-zone per-cpu lists this
    patch prints the sum of these counters for each zone (free_pcp) and size
    of per-cpu list for current cpu (local_pcp).
    
    The filter flag SHOW_MEM_PERCPU_LISTS reverts to the old verbose mode.
    
    [akpm@linux-foundation.org: update show_free_areas comment]
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	include/linux/mm.h

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -893,6 +893,7 @@ extern void pagefault_out_of_memory(void);
  */
 #define SHOW_MEM_FILTER_NODES		(0x0001u)	/* disallowed nodes */
 #define SHOW_MEM_FILTER_PAGE_COUNT	(0x0002u)	/* page type count */
+#define SHOW_MEM_PERCPU_LISTS		(0x0003u)	/* per-zone per-cpu */
 
 extern void show_free_areas(unsigned int flags);
 extern bool skip_free_areas_node(unsigned int flags, int nid);
