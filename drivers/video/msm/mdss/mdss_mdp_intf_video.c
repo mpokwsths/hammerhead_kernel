CAF-staging/msm8916_kernel

mdss: display: unmap 'fb_addr_va' when the mapping is no longer needed
Remove the mapping to the original continuous splash frame buffer
once its contents have been copied. This avoids a virtual address
leak, and holding a mapping to memory that may be eventually
reclaimed for use by other subsystem.

Change-Id: I8d7b6d954809a74d5a1ae60491e295b95458a2b7
Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

drivers/video/msm/mdss/mdss_mdp_intf_video.c
@@ -680,8 +680,8 @@ int mdss_mdp_video_copy_splash_screen(struct mdss_panel_data *pdata)
 			(unsigned long int)virt, &phys);
 
 	fb_addr_va = (unsigned long *)ioremap(fb_addr, size);
-
 	memcpy(virt, fb_addr_va, size);
+	iounmap(fb_addr_va);
 
 	sp = splash_pipes;
 	flush = 0;

mpokwsths/hammerhead_kernel

drivers/video/msm/mdss/mdss_mdp_intf_video.c
@@ -551,8 +551,8 @@ int mdss_mdp_video_copy_splash_screen(struct mdss_panel_data *pdata)
 			(unsigned long int)virt, &phys);
 
 	bl_fb_addr_va = (unsigned long *)ioremap(bl_fb_addr, size);
-
 	memcpy(virt, bl_fb_addr_va, size);
+	iounmap(bl_fb_addr_va);
 
 	MDSS_MDP_REG_WRITE(pipe_addr, phys);
 	MDSS_MDP_REG_WRITE(MDSS_MDP_REG_CTL_FLUSH + MDSS_MDP_REG_CTL_OFFSET(0),